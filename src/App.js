a4afd917c2739f002f4bdbaea1b95d749fd36494
miuraK <kanae9@icloud.com>
2021-01-27 15:23:18 +0900

    チェックしたもののみを引っ張る,データファイルを追加等

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
-import * as d3 from "d3";
-import * as topojson from "topojson";
-import { areaRadial } from "d3";
-
-import RankingData from "./data.json"
-const rData = RankingData;
-//console.log(rData);
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { AboutAwareness } from "./Awareness";
+import { AboutLocalResources } from "./Resouces";
 
 const Hero = () => {
     return (
@@ -41,319 +37,54 @@ const AboutData = () => {
     );
 }
 
-//日本地図を描くプログラム
-const ChoroplethMap = ({ features }) => {
-  const width = 1000;
-  const height = 900;
-  const projection = d3.geoMercator().scale(1600).center([139.69167, 35.68944]).translate([width/2, height/2]);
-  const path = d3.geoPath().projection(projection);
-  const color = d3
-    .scaleLinear()
-    .domain(d3.extent(features, (feature) => feature.properties.value))
-    .range(["#ccc", "#0f0"]);
-  
-  //印
-  const radius = 25;
-  //米のみの場合で考えると？
-  const rData = RankingData;
-  const rice = rData.map((item) => {
-    const obj = {
-      都道府県:item.都道府県名,
-      米:item.米,
-      x:item.経度,
-      y:item.緯度
-    }
-    return obj;
-  });
-  //降順にソート 
-  const r = rice.sort((a,b) => (b.米) - (a.米));
-  //console.log(r);
-  //〜印
-
-  return (
-    <svg width={width} height={height}>
-      <g>
-        {features.map((feature, i) => (
-          <path
-            key={i}
-            d={path(feature)}
-            fill={color(feature.properties.value)}
-            stroke="#7F7F7F"
-          />
-        ))}
-      </g>
-      
-      {/*印の大きさについて*/}
-        <g transform="translate(width/2,height)">
-            <text x={width-250} y={height-500} fontSize="20">円の大きさ・色について</text>
-
-            <circle cx={width-250} cy={height-450} r={radius-5} fill="red" opacity="0.5"/>
-            <text x={width-250+radius} y={height-450} fontSize="20">1位</text>
-
-            <circle cx={width-250} cy={height-400} r={radius-5} fill="blue" opacity="0.5"/>
-            <text x={width-250+radius} y={height-400} fontSize="20">2位</text>
-
-            <circle cx={width-250} cy={height-350} r={radius-5} fill="yellow" opacity="0.5"/>
-            <text x={width-250+radius} y= {height-350}fontSize="20">3位</text>
-
-            <circle cx={width-250} cy={height-300} r={radius-10} fill="black" opacity="0.5"/>
-            <text x={width-250+radius-10} y={height-300} fontSize="20">3~10位</text>
-
-            <circle cx={width-250} cy={height-250} r={radius-15} fill="black" opacity="0.5"/>
-            <text x={width-250+radius-15} y={height-250} fontSize="20">10~20位</text>
-
-            <circle cx={width-250} cy={height-200} r={radius-18} fill="black" opacity="0.5"/>
-            <text x={width-250+radius-18} y={height-200} fontSize="20">20~30位</text>
-
-            <circle cx={width-250} cy={height-150} r={radius-20} fill="black" opacity="0.5"/>
-            <text x={width-250+radius-20} y={height-150} fontSize="20">30~40位</text>
-        </g>
-        {/*〜印の大きさについて*/}
-
-        {/*印（米のみの場合）*/}
-        <g>
-          {rice.map((item,i) => {
-            //x=経度 y=緯度
-            const x = projection([rice[i].x, rice[i].y])[0];
-            const y = projection([rice[i].x, rice[i].y])[1];
-            //console.log(x,y);
-            return <circle cx={x} cy={y} r={radius} fill="red" opacity="0.5"/>;
-          })}
-        </g>
-        {/*〜印（米のみの場合）*/}
-    </svg>
-  );
-};
-export const ChoroplethMapPage = () => {
-  const [features, setFeatures] = useState(null);
-  useEffect(() => {
-    (async() => {
-      const res = await fetch(`${process.env.PUBLIC_URL}/data/japan.json`)
-      const data = await res.json()
-      const { features } = topojson.feature(data, data.objects.japan);
-      setFeatures(features);
-    })()
-  },[])
-  if (features == null) {
-    return <p>loading</p>;
-  }
-  return <ChoroplethMap features={features} />;
-};
-//〜日本地図を描くプログラム
-
-//地域資源についてのプログラム
-const AboutLocalResources = () => {
-  const checkrist = ["火力","水力","風力","原子力","太陽光","地熱","米","牛乳","肉用牛","豚","鶏卵","プロイラー","トマト","乳牛","いちご"];
-  const [val, setVal] = React.useState([]);
-  const handleChange = e => {    //ONかOFFか
-    if (val.includes(e.target.value)) { // すでに含まれていればOFFしたと判断し、イベント発行元を除いた配列をsetし直す (チェックなし)
-      setVal(val.filter(item => item !== e.target.value));
-    } else { // そうでなければONと判断し、イベント発行元を末尾に加えた配列をsetし直す (チェックあり)
-      setVal([...val, e.target.value]); // stateは直接は編集できない( = val.push(e.target.value) はNG)
-    }
-  };
-  console.log(val);
-
-  {/*考え中！！
-  let ranking = [];
-  let r = [];
-  r.push(rData.map((item) => {
-    //console.log(item[e.target.value]);
-    return item[e.target.value];
-  }));
-  console.log('チェックしたもののみ');
-  console.log(r);
-  
-  ranking.push(r);
-  console.log('チェックしたもを配列に');
-  console.log(ranking);
-  //考え中！！
-  */}
-
-  return ( 
-    <div className="tile is-parent is-vertical">
-      <article className="tile is-child notification is-grey has-text-centered">
-        <div>
-        <form onreset = "reset">
-        {checkrist.map((item,i) => {
-          if(i === 5){
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-                <br />
-              </label>
-            );
-          }else{
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-              </label>
-            );
-          }
-        })}
-        <br/>
-        <button type = "reset">描画</button>
-        </form>
-        </div>
-        <ChoroplethMapPage /> 
-      </article>
-    </div>
-  );
-}
-
-//認知度についてのプログラム
-const AboutAwareness = () => {
-  const ranking = [];
-  const checkrist = ["Youtube登録者数","Youtube最高再生数","Twitterフォロワー数","Twitterツイート数","Twitter開始年月","全国国内線乗降客数","外国人訪問率","芸能人"];
-
-  const [val, setVal] = React.useState([]);
-  const handleChange = e => {    //ONかOFFか
-    if (val.includes(e.target.value)) { // すでに含まれていればOFFしたと判断し、イベント発行元を除いた配列をsetし直す
-      setVal(val.filter(item => item !== e.target.value));
-    } else { // そうでなければONと判断し、イベント発行元を末尾に加えた配列をsetし直す (チェックあり)
-      setVal([...val, e.target.value]); // stateは直接は編集できない( = val.push(e.target.value) はNG)
-
-    }
-  };
-  console.log(val);
-
-  return ( 
-    <div className="tile is-parent is-vertical">
-      <article className="tile is-child notification is-grey has-text-centered">
-        <div>
-        <form onreset = "reset">
-        {checkrist.map((item,i) => {
-          if(i === 4){
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-                <br />
-              </label>
-            );
-          }else{
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-              </label>
-            );
-          }
-        })}
-        <br/>
-        <button type = "reset">描画</button>
-        </form>
-        </div>
-        <ChoroplethMapPage /> 
-      </article>
-    </div>
-  );
-}
-
-//総合点についてのプログラム
-const AboutTotal = () => {
-  const data = []
-  const checkrist = ["火力","水力","風力","原子力","太陽光","地熱","米","牛乳","肉用牛","豚","鶏卵","プロイラー","トマト","乳牛","いちご","Youtube登録者数","Youtube最高再生数","Twitterフォロワー数","Twitterツイート数","Twitter開始年月","全国国内線乗降客数","外国人訪問率","芸能人"];
-
-  const [val, setVal] = React.useState([]);
-  const handleChange = e => {    //ONかOFFか
-    if (val.includes(e.target.value)) { // すでに含まれていればOFFしたと判断し、イベント発行元を除いた配列をsetし直す
-      setVal(val.filter(item => item !== e.target.value));
-    } else { // そうでなければONと判断し、イベント発行元を末尾に加えた配列をsetし直す (チェックあり)
-      setVal([...val, e.target.value]); // stateは直接は編集できない( = val.push(e.target.value) はNG)
-    }
-  };
-  console.log(val);
-
-  return ( 
-    <div className="tile is-parent is-vertical">
-      <article className="tile is-child notification is-grey has-text-centered">
-        <div>
-        <form onreset = "reset">
-        {checkrist.map((item,i) => {
-          if(i === 5 || i === 14 || i === 19){
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-                <br />
-              </label>
-            );
-          }else{
-            return (
-              <label>
-                <input type="checkbox" value={item} onChange={handleChange} checked={val.includes(item)}/>
-                {item}
-              </label>
-            );
-          }
-        })}
-        <br/>
-        <button type = "reset">描画</button>
-        </form>
-        </div>
-        <ChoroplethMapPage /> 
-      </article>
-    </div>
-  );
-}
-
-const Button = (nowShowing) => {
-  //console.log(nowShowing);
-  if (nowShowing.value === 1) {
-    return (<AboutTotal />);
-  } else if (nowShowing.value === 2) {
-    return (<AboutLocalResources />);
-  } else {
-    return (<AboutAwareness />);
-  }
-};
-
 const App = () => {
-  const [nowShowing, setNowShowing] = useState(1);
-    return (
-      <div>
-        <Hero />
-        <section className="section">
-            <div className="buttons is-centered">
-                <button className="button is-info is-large is-outlined" onClick={() => setNowShowing(1)}>総合点</button>
-                <button className="button is-primary is-large is-outlined" onClick={() => setNowShowing(2)}>地域資源</button>
-                <button className="button is-success is-large is-outlined" onClick={() => setNowShowing(3)}>認知度</button>
-                <Button value={nowShowing} />
+  return (
+    <Router>
+      <Hero />
+      <div className="notification is-primary">
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical">
+            <div className="tile is-parent">
+              <article className="tile is-child box">
+                <div className="content">
+                  <section className="section">
+                   <div className="buttons is-centered">
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                      <button className="button is-link is-large is-outlined">
+                        <p className="title is-1">1 総合点</p>
+                      </button>
+                    </Link>
+                    <Link to="/local" style={{ textDecoration: "none" }}>
+                      <button className="button is-primary is-large is-outlined">
+                        <p className="title is-1">2 地域資源</p>
+                      </button>
+                    </Link>
+                    <Link to="/awareness" style={{ textDecoration: "none" }}>
+                      <button className="button is-danger is-large is-outlined">
+                        <p className="title is-1">3 認知度</p>
+                      </button>
+                    </Link>
+                   </div>
+                    <Route path="/" exact>
+                      {/*<AboutTotal />*/}
+                    </Route>
+                    <Route path="/local" exact>
+                      <AboutLocalResources />
+                    </Route>
+                    <Route path="/awareness" exact>
+                      <AboutAwareness />
+                    </Route>
+                  </section>
+                </div>
+              </article>
             </div>
-        </section>
-        <AboutData />
-        <Footer />
+          </div>
+        </div>
       </div>
-    );
+      <AboutData />
+      <Footer />
+    </Router>
+  )
 };
 
-
-/*(メモ用)
-//全体のみの場合は？
-const data = []
-  data.push(rData.map((item) => {
-    const obj = {
-      都道府県名:item.都道府県名 
-    }
-    return obj;
-  }))
-  console.log(data);
-
-//米のみの場合で考えると？
-const rice = rData.map((item) => {
-  const obj = {
-    都道府県:item.都道府県名,
-    米:item.米,
-    x:item.経度,
-    y:item.緯度
-  }
-  return obj;
-});
-//降順にソート 
-const r = rice.sort((a,b) => (b.米) - (a.米));
-*/
-  
 export default App;
---
+++ src/Awareness.js
@@ -0,0 +1,257 @@
+import React, { useEffect, useState } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson";
+
+//日本地図を描くプログラム
+const ChoroplethMap = ({ features }) => {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+  const dataUrl = `${process.env.PUBLIC_URL}/data/normalized_data.json`;
+
+  const selections = ["Youtube登録者数","Youtube最高再生数","Twitterフォロワー数","Twitterツイート数","Twitter開始年月","国内線乗降客数","外国人訪問率","芸能人"];
+
+  const width = 900;
+  const height = 900;
+  const radius = 25;
+
+  const projection = d3
+    .geoMercator()
+    .scale(1600)
+    .center([139.69167, 35.68944])
+    .translate([width / 2, height / 2]);
+  const path = d3.geoPath().projection(projection);
+  const color = d3
+    .scaleLinear()
+    .domain(d3.extent(features, (feature) => feature.properties.value))
+    .range(["#ccc", "#0f0"]);
+
+  useEffect(() => {
+    async function fetchData(dataUrl) {
+      const res = await fetch(dataUrl);
+      const json = await res.json();
+      const data = json.data;
+
+      setData(data);
+    }
+
+    fetchData(dataUrl);
+  }, []);
+
+  useEffect(() => {
+    setData((prev) => [
+      ...prev.sort((a, b) => {
+        return (
+          selected.reduce((acc, cur) => acc + b[cur].normalizedValue, 0) -
+          selected.reduce((acc, cur) => acc + a[cur].normalizedValue, 0)
+        );
+      }),
+    ]);
+  }, [selected]);
+
+  return (
+    <div className="box">
+        <form>
+        <div className="field">
+        <div className="control">
+            {selections.map((selection) => {
+              return (
+                <label className="label" key={selection}>
+                  <input type="checkbox" value={selection}
+                    onChange={(e) =>
+                      setSelected((prev) => {
+                        if (e.target.checked) {
+                          return prev.concat(e.target.value);
+                        } else {
+                          return prev.filter((item) => item !== e.target.value);
+                        }
+                      })
+                    }
+                  />
+                  {selection}
+                </label>
+              );
+            })}
+        </div>
+        </div>
+      </form>
+      <svg width={width} height={height}>
+        <g> 
+          {features.map((feature, i) => (
+            <path
+              key={i}
+              d={path(feature)}
+              fill="limegreen"
+              stroke="mediumseagreen"
+            />
+          ))}
+        </g>
+
+        {/*印の大きさについて*/}
+        <g transform="translate(width/2,height)">
+          <text x={width - 250} y={height - 500} fontSize="20">
+            円の大きさ・色について
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 450}
+            r={radius - 5}
+            fill="red"
+            opacity="0.5"
+          />
+          <text x={width - 200 + radius} y={height - 443} fontSize="20">
+            1位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 400}
+            r={radius - 5}
+            fill="blue"
+            opacity="0.5"
+          />
+          <text x={width - 200 + radius} y={height - 393} fontSize="20">
+            2位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 350}
+            r={radius - 5}
+            fill="yellow"
+            opacity="0.5"
+          />
+          <text x={width - 200 + radius} y={height - 343} fontSize="20">
+            3位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 300}
+            r={radius - 10}
+            fill="pink"
+            opacity="0.5"
+          />
+          <text x={width - 195 + radius - 10} y={height - 294} fontSize="20">
+            4~10位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 250}
+            r={radius - 15}
+            fill="black"
+            opacity="0.5"
+          />
+          <text x={width - 195 + radius - 15} y={height - 243} fontSize="20">
+            11~20位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 200}
+            r={radius - 18}
+            fill="black"
+            opacity="0.5"
+          />
+          <text x={width - 195 + radius - 18} y={height - 193} fontSize="20">
+            21~30位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 150}
+            r={radius - 20}
+            fill="black"
+            opacity="0.5"
+          />
+          <text x={width - 195 + radius - 20} y={height - 143} fontSize="20">
+            31~40位
+          </text>
+
+          <circle
+            cx={width - 200}
+            cy={height - 100}
+            r={radius - 22}
+            fill="black"
+            opacity="0.5"
+          />
+          <text x={width - 195 + radius - 22} y={height - 93} fontSize="20">
+            41~47位
+          </text>
+        </g>
+        {/*〜印の大きさについて*/}
+
+        <g>
+          {selected.length !== 0 &&
+            data.map((item, index) => {
+              const x = projection([
+                data[index]["経度"],
+                data[index]["緯度"],
+              ])[0];
+              const y = projection([
+                data[index]["経度"],
+                data[index]["緯度"],
+              ])[1];
+
+              let r = 0;
+              let color = "red";
+
+              if (index == 0) {
+                r = radius - 5;
+                color = "red";
+              } else if (index == 1) {
+                r = radius - 5;
+                color = "blue";
+              } else if (index == 2) {
+                r = radius - 5;
+                color = "yellow";
+              } else if (index <= 10) {
+                r = radius - 10;
+                color = "black";
+              } else if (index <= 20) {
+                r = radius - 15;
+                color = "black";
+              } else if (index <= 30) {
+                r = radius - 18;
+                color = "black";
+              } else if (index <= 40) {
+                r = radius - 20;
+                color = "black";
+              } else {
+                r = radius - 22;
+                color = "black";
+              }
+              return (
+                <circle
+                  cx={x}
+                  cy={y}
+                  r={r}
+                  fill={color}
+                  opacity="0.5"
+                  key={item["都道府県名"]}
+                />
+              );
+            })}
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export const AboutAwareness = () => {
+  const [features, setFeatures] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${process.env.PUBLIC_URL}/data/japan.json`);
+      const data = await res.json();
+      const { features } = topojson.feature(data, data.objects.japan);
+      setFeatures(features);
+    })();
+  }, []);
+  if (features == null) {
+    return <p>loading</p>;
+  }
+  return <ChoroplethMap features={features} />;
+};
